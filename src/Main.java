482c614fdcd8ce1ba9dc5d40d392b7fcf301fb71
Sarah Risma <sarahgrace.risma@calbaptist.edu>
2024-03-22 10:31:00 -0700

    added loading obj algorithm and mandelbrot code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,81 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Vector;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public static Vector<Float> vertices = new Vector<>();
+    public static Vector<Integer> indices = new Vector<>();
+    public static Vector<Float> normals = new Vector<>();
+
+    public static void main(String[] args) throws IOException{
+        String objFile = "obj/teapot.obj";
+        loadObj(objFile);
+        System.out.println("\nFinished loading obj file");
     }
+
+    // this is just to try and see if this actually works haha
+    // from ChatGPT currently
+    private static void loadObj(String filename) throws IOException {
+        System.out.println("Start loading obj file");
+//        ArrayList<Float> vertices = new ArrayList<>();
+//        ArrayList<Integer> indices = new ArrayList<>();
+//        ArrayList<Float> normals = new ArrayList<>();
+//        ArrayList<Float> textCoords = new ArrayList<>();
+        try {
+           BufferedReader reader = new BufferedReader(new FileReader(filename));
+           String line;
+
+           while ((line = reader.readLine()) != null) {
+               String[] components = line.split("\\s+");
+               if (line.startsWith("v ")) {
+                   vertices.add(Float.parseFloat(components[1]));
+                   vertices.add(Float.parseFloat(components[2]));
+                   vertices.add(Float.parseFloat(components[3]));
+               } else if (line.startsWith("f " )) {
+                   for (int i = 1; i < 4; i++) { // assuming triangular faces
+                       String[] vertex = components[i].split("/");
+                       indices.add((Integer.parseInt(vertex[0]) - 1)); // vertex indices start from 1 in ojb, so subtract 1
+                   }
+               }
+           }
+           reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        System.out.println("Vertices size: " + vertices.size());
+//        System.out.println("Indices size: " + indices.size());
+        printFloat("vertices", vertices);
+//        printFloatArrayList("normals", normals);
+//        printFloatArrayList("texture", textCoords);
+        printInt("indices", indices);
+    }
+
+    // takes in float array list
+    private static void printFloat(String type, Vector<Float> A) {
+        System.out.println("\n" + type + ":");
+        for (int i = 0; i < A.size(); i++) {
+            if (i == A.size() - 1) {
+                System.out.println(A.get(i));
+            } else {
+                System.out.print(A.get(i) + ", ");
+            }
+        }
+    }
+
+    // takes in integer array list
+    private static void printInt(String type, Vector<Integer> A) {
+        System.out.println("\n" + type + ":");
+        for (int i = 0; i < A.size(); i++) {
+            if (i == A.size() - 1) {
+                System.out.println(A.get(i));
+            } else {
+                System.out.print(A.get(i) + ", ");
+            }
+        }
+    }
+
 }
---
+++ src/Mandelbrot.java
@@ -0,0 +1,100 @@
+import java.util.Vector;
+
+public class Mandelbrot {
+    public static Boolean found(Vector<ImgNum> data, ImgNum test) {
+        Boolean ret = false;
+        for (int i = 0; i < data.size(); i++) {
+            if (data.elementAt(i).iEq(test))
+                ret = true;
+        }
+        return ret;
+    }
+
+    // c is of the form c= x + iy
+    // i is the square root of -1
+    public static Integer Mandelbrot(Integer threshold, Double cx, Double cy) {
+        Integer ret = 0;
+
+        Vector<ImgNum> prevZ = new Vector<ImgNum>();
+        ImgNum zCurrent = new ImgNum(0.0, 0.0);
+        ImgNum c = new ImgNum(cx, cy);
+
+        Integer steps = 0;
+
+        while (!found(prevZ, zCurrent) && steps < threshold) {
+            prevZ.add(zCurrent);
+            zCurrent = zCurrent.iMult(zCurrent);
+            zCurrent = zCurrent.iAdd(c);
+
+            steps++;
+        }
+
+        if (found(prevZ, zCurrent)) {
+            ret = steps;
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        for (Double i = -2.0; i <= 2.0; i += 0.10) {
+            for (Double j = -2.0; i <= 2.0; j += 0.05) {
+                Integer steps = Mandelbrot(30, j, i);
+                if (steps.equals(0)) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
+    // imaginary number class
+    static class ImgNum {
+        // c = x + iy
+        Double x;
+        Double y;
+
+        public ImgNum() {
+            x = 0.0;
+            y = 0.0;
+        }
+
+        public ImgNum(Double inputx, Double inputy) {
+            x = inputx;
+            y = inputy;
+        }
+
+        public ImgNum iMult(ImgNum A) {
+            // (x + y i) (A.x + A.y i)
+            // x A.x + (x A.y + y A.x) i - y A.y
+            // x A.x - yA.y + (x A.y + y A.x)i
+
+            ImgNum ret = new ImgNum();
+            ret.x = x * A.x - y * A.y;
+            ret.y = x * A.y + y * A.x;
+
+            return ret;
+        }
+
+        public ImgNum iAdd(ImgNum A) {
+            ImgNum ret = new ImgNum();
+            ret.x = x + A.x;
+            ret.y = y + A.y;
+
+            return ret;
+        }
+
+        public Boolean iEq(ImgNum A) {
+            if (x.equals(A.x) && y.equals(A.y)) {
+                return true;
+            }
+            return false;
+        }
+
+        public void iPrint() {
+            System.out.print(x + " + i" + y);
+        }
+    }
+}
